70af8f2b751b57061c4d1f226db4d0636656d01d
Alex <alex.rodrigues23997@gmail.com>
2022-12-31 04:07:03 -0300

    Small mistake

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -6,4 +6,4 @@ docker login -u azold6 -p $PASS
 
 docker build -t azold6/jenkins-with-spring:$BUILD_TAG .
 
-docker push azold6/jenkins-with-spring:BUILD_TAG
+docker push azold6/jenkins-with-spring:$BUILD_TAG
