e18d33b3ac801050c8e35891720ec67551eb0db6
Alex <alex.rodrigues23997@gmail.com>
2023-01-01 22:05:53 -0300

    Update msh

--- src/main/java/com/azl6/testforjenkins/controllers/HelloController.java
+++ src/main/java/com/azl6/testforjenkins/controllers/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 
     @GetMapping
     public ResponseEntity<String> sayHello(){
-        return ResponseEntity.ok().body("TESTANDO A PIPELINE!");
+        return ResponseEntity.ok().body("Mensagem base.");
     }
     
 }
